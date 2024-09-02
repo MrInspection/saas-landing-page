4151c860abe73fefa613dce297e22e38233d78ea
〽️oussa <155066673+MrInspection@users.noreply.github.com>
2024-09-02 21:38:17 +0200

    Added OG Images

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,16 +8,6 @@ const dmSans = DM_Sans({subsets: ["latin"]});
 export const metadata: Metadata = {
     title: "SaaS Landing Page",
     description: "A beautiful dark landing page for a SaaS application.",
-    openGraph: {
-        images: [
-            {
-                url: "/spectronlabs.png",
-                width: 1200,
-                height: 630,
-                alt: "Moussax • SaaS Landing Page",
-            },
-        ],
-    }
 };
 
 export default function RootLayout({children}: Readonly<{ children: React.ReactNode }>) {
