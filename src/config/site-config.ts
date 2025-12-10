525e95639dbc1fa15798a299872593648f7cfbc0
Moussa <moussaabdallahhoumadi@gmail.com>
2025-12-10 11:36:18 +0100

    refactor: update project metadata

---
+++ src/config/site-config.ts
@@ -0,0 +1,20 @@
+export const siteConfig = {
+  name: "SaaS Landing Page",
+  description: "A beautiful dark landing page for a SaaS application.",
+  creator: "MrInspection",
+  links: {
+    repositoryUrl: "https://github.com/MrInspection/saas-landing-page",
+    creatorGithubUrl: "https://github.com/MrInspection/",
+    deploymentUrl: "https://splabs-saas-lp.vercel.app/",
+  },
+  openGraph: {
+    imageUrl: "https://splabs-saas-lp.vercel.app/og-image.png",
+    imageWidth: 1200,
+    imageHeight: 630,
+  },
+  twitter: {
+    creator: "@MrInspection",
+    cardType: "summary_large_image",
+    imageUrl: "https://splabs-saas-lp.vercel.app/og-image.png",
+  },
+};
